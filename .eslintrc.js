baaf4c396933ba1893890ebcc38aec9796e7a697
Amelia Wibisono <ameliawibi@gmail.com>
2021-07-26 08:58:24 +0800

    rm eslint extends react-app

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["airbnb", "plugin:import/recommended", "prettier", "react-app"],
+  extends: ["airbnb", "plugin:import/recommended", "prettier"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
