7daf772b4fea7af44f670fb3b162e6998d7f24d7
Amelia Wibisono <ameliawibi@gmail.com>
2021-08-05 22:43:17 +0800

    Add template for aboutme paragraph

---
+++ src/components/Aboutme/Aboutme.js
@@ -0,0 +1,91 @@
+function Aboutme() {
+  return (
+    <section
+      id="aboutme"
+      className="container flex grid grid-flow-row grid-cols-1 lg:grid-cols-2 py-8 w-4/5 m-auto"
+    >
+      <div className="m-8 hover:text-white">
+        <div>
+          <h1 className="text-6xl text-gray-700 tracking-wider">1991</h1>
+          <h2 className="-mt-6 text-2xl">
+            CHAPTER I: <br /> BORN INTO THIS WORLD
+          </h2>
+        </div>
+        <div className="ml-2 mt-10">
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+            sed quia non numquam eius modi tempora incidunt ut labore et dolore
+            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
+            nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+            aliquid ex ea commodi consequatur? Quis autem vel eum iure
+            reprehenderit qui in ea voluptate velit esse quam nihil molestiae
+            consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
+            pariatur?
+          </p>
+        </div>
+      </div>
+
+      <div className="m-8 hover:text-white">
+        <div>
+          <h1 className="text-6xl text-gray-700 tracking-wider">2006</h1>
+          <h2 className="-mt-6 text-2xl">
+            CHAPTER II: <br /> OFF TO SINGAPORE
+          </h2>
+        </div>
+        <div className="ml-2 mt-10">
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+            sed quia non numquam eius modi tempora incidunt ut labore et dolore
+            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
+            nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+            aliquid ex ea commodi consequatur? Quis autem vel eum iure
+            reprehenderit qui in ea voluptate velit esse quam nihil molestiae
+            consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
+            pariatur?
+          </p>
+        </div>
+      </div>
+
+      <div className="m-8 hover:text-white">
+        <div>
+          <h1 className="text-6xl text-gray-700 tracking-wider"> 2015 </h1>
+          <h2 className="-mt-6 text-2xl">
+            CHAPTER III: <br /> PREVIOUS LIFE
+          </h2>
+        </div>
+        <div className="ml-2 mt-10">
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+            sed quia non numquam eius modi tempora incidunt ut labore et dolore
+            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
+            nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+            aliquid ex ea commodi consequatur? Quis autem vel eum iure
+            reprehenderit qui in ea voluptate velit esse quam nihil molestiae
+            consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
+            pariatur?
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Aboutme;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -3,11 +3,13 @@ import Header from "../../components/Header/Header";
 import Certs from "../../components/Certs/Certs";
 import Whyme from "../../components/Whyme/Whyme";
 import Contact from "../Contact/Contact";
+import Aboutme from "../../components/Aboutme/Aboutme";
 
 function Home() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
       <Header />
+      <Aboutme />
       <Certs />
       <Whyme />
       <Contact />
